0320fadd9ee6a14c4d0f976fb7cc6d85e78c862d
omegahanggara <hanggara33@gmail.com>
2014-03-24 16:53:08 +0700

    Added scripts/setup/setup.sh at Mon Mar 24 16:53:08 WIB 2014

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -11,6 +11,28 @@ function check_cfg () {
                cout action "Checking $var in $FILES"
                if [[ $(cat $FILES | grep $var | awk -F "=" '{print $2}') == "" ]]; then
                     cout warning "Warning!!! Empty value $var at $FILES"
+                    ask_to_setup_var="true"
+                    while [[ $ask_to_setup_var == "true" ]]; do
+                         cin info "Do you want to setup $var value? (Y/n)"
+                         read answer_to_setup_var
+                         if [[ $answer_to_setup_var == *[Yy]* || $answer_to_setup_var == "" ]]; then
+                              ask_to_setup_var="false"
+                              cout action "Setting up $var..."
+                              if [[ $var == "PASSWORD" ]]; then
+                                   read -s -p "Enter Password: " mypassword
+                                   echo
+                                   cout info "Your password is $mypassword"
+                                   cout action "Adding your password to configuration..."
+                                   sed -i "s/PASSWORD=/PASSWORD=$mypassword/g" $FILES
+                              fi
+                         elif [[ $answer_to_setup_var == *[Nn]* ]]; then
+                              ask_to_setup_var="false"
+                              cout warning "This value can't be empy!!! Exiting!!!"
+                              exit 1
+                         else
+                              cout warning "Whoops... Typo? Try harder!"
+                         fi
+                    done
                else
                     cout info "So far so good..."
                     cout info "Your configuration for $var is $(cat $FILES | grep $var | awk -F "=" '{print $2}')"
