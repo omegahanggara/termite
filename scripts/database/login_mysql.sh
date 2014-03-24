8cb1d6252a7f6a18f2f704131baca5a47768f4ec
omegahanggara <hanggara33@gmail.com>
2014-03-24 12:14:42 +0700

    Renice scripts/database/login_mysql.sh at Mon Mar 24 12:14:42 WIB 2014

--- scripts/database/login_mysql.sh
+++ scripts/database/login_mysql.sh
@@ -2,7 +2,7 @@
 
 function check_login() {
      cout action "Try to login to mysql as `whoami` at $HOST..."
-     mysql_ver="$(mysql -u root -h $HOST -p -e 'SELECT VERSION();' | tail -1)"
+     mysql_ver="$(mysql -u $USER -h $HOST -p -e 'SELECT VERSION();' | tail -1)"
      cout info "Your mysql version is $mysql_ver"
      cout info "Done..."
 }
