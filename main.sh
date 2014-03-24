534c98f672985335816689fc8392b978ab409c75
omegahanggara <hanggara33@gmail.com>
2014-03-24 12:11:49 +0700

    Included config directory to source at Mon Mar 24 12:11:49 WIB 2014

--- main.sh
+++ main.sh
@@ -4,6 +4,10 @@ for SOURCES in $(find ./scripts -type f -name "*.sh"); do
      source $SOURCES
 done;
 
+for SOURCES in $(find ./config -type f -name "*.cfg"); do
+     source $SOURCES
+done;
+
 checkroot
 report_detect_mysql
 check_login
