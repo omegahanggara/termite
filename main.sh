40f50f90f10574b60e9ee6f740e62c268fa2e9d1
omegahanggara <hanggara33@gmail.com>
2014-03-27 13:28:38 +0700

    Inserted check_backup_folder in main.sh at Thu Mar 27 13:28:38 WIB 2014

--- main.sh
+++ main.sh
@@ -4,8 +4,8 @@ for SOURCES in $(find ./scripts -type f -name "*.sh"); do
      source $SOURCES
 done;
 
-for SOURCES in $(find ./config -type f -name "*.cfg"); do
-     source $SOURCES
+for CONFIG in $(find ./config -type f -name "*.cfg"); do
+     source $CONFIG
 done;
 
 check_cfg
@@ -13,3 +13,4 @@ checkroot
 report_detect_mysql
 check_login
 num_of_databases
+check_backup_folder
