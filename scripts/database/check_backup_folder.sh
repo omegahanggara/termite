f32901770084f1da43e867ceecf0b8d0724fd0fb
omegahanggara <hanggara33@gmail.com>
2014-03-27 13:36:25 +0700

    Added check_backup_folder_helper function in scripts/database/check_backup_folder.sh at Thu Mar 27 13:36:25 WIB 2014

--- scripts/database/check_backup_folder.sh
+++ scripts/database/check_backup_folder.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+function check_backup_folder_helper () {
+     ask_to_mkdir_backup_folder="true"
+     while [[ $ask_to_mkdir_backup_folder == "true" ]]; do
+          cin info "Would you like to create $PATH_BACKUP? (Y/n) : "
+          read answer_to_mkdir_backup_folder
+          if [[ $answer_to_mkdir_backup_folder == *[Yy]* || $answer_to_mkdir_backup_folder == "" ]]; then
+               ask_to_mkdir_backup_folder="false"
+               cout action "Create new directory on $PATH_BACKUP..."
+               mkdir -p $PATH_BACKUP
+               cout info "Done"
+          elif [[ $answer_to_mkdir_backup_folder == *[Nn]* ]]; then
+               ask_to_mkdir_backup_folder="false"
+               cout error "Please create folder on $PATH_BACKUP first, then run this script again!"
+               exit 1
+          else
+               cout error "Whoops... Typo? Try harder!"
+          fi
+     done
+}
+
 function check_backup_folder () {
      cout info "Your default backup path is $PATH_BACKUP"
      cout action "Checking backup folder on $PATH_BACKUP"
@@ -7,6 +27,6 @@ function check_backup_folder () {
           cout info "$PATH_BACKUP is found"
      else
           cout warning "$PATH_BACKUP is not found!!!"
-          exit 1
+          check_backup_folder_helper
      fi
 }
