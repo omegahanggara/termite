524167bf99c11b725a23cd8c5526881f945f8f9a
omegahanggara <hanggara33@gmail.com>
2014-03-27 13:27:10 +0700

    Added PASSWORD to skip password answer at Thu Mar 27 13:27:10 WIB 2014

--- scripts/database/login_mysql.sh
+++ scripts/database/login_mysql.sh
@@ -2,11 +2,11 @@
 
 function check_login() {
      cout action "Try to login to mysql as `whoami` at $HOST..."
-     mysql_ver="$(mysql -u $USER -h $HOST -p -e 'SELECT VERSION();' | tail -1)"
+     mysql_ver="$(mysql -u $USER -h $HOST -p$PASSWORD -e 'SELECT VERSION();' | tail -1)"
      cout info "Your mysql version is $mysql_ver"
      cout info "Done..."
 }
 
 function sql_cmd() {
-     mysql -u $USER -h $HOST -p -e "$1"
+     mysql -u $USER -h $HOST -p$PASSWORD -e "$1"
 }
